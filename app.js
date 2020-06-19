d7b9f76f40b9d8a11e2615023f3a89ff2e679b3a
Jonathan <jonathan.george.dev@gmail.com>
2020-06-18 23:21:07 -0700

    UI controller update addlistItems

--- app.js
+++ app.js
@@ -41,15 +41,30 @@ var budgetController = (function() {
        //type == inc or exp
        addItem: function(type, des, val){
             var newItem, ID;
-            ID = 0;
+
+            //create new id 
+            if(data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1
+            } else {
+                ID = 0
+            }
+       
+       
+
+            //create a new item based on inc or exp type
             if(type === 'exp'){
                 newItem = new Expense(ID, des, val);
             } else if(type === 'inc'){
                 newItem = new Income(ID, des, val);
             }   
-
+            //pushees into our data structure 
             data.allItems[type].push(newItem);
+            //returns new element 
             return newItem;
+       },
+       //testing/bedugeing. in console type budgetContoller.testing
+       testing: function(){
+           console.log(data)
        }
    }
 
@@ -75,7 +90,30 @@ var UIController = (function() {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
-        
+        //same as using function concruture and them pass to app controller?? 
+        addListItem: function(obj, type) {
+            var html;
+            //create HTML string with placeholder text
+            if(type === 'inc'){
+                //%id%, %description%.... is the place holder tags, and is easy to find %%
+                html ='<div class="item clearfix" id="income-%id%"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else {
+                html ='<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+            
+            //Replace the placeholder text woth some  actual data
+            //replace method....html is a string so we can manipulat that string
+            newHtml = html.replace('%id%', onj.id) //from budgetController (id, description, value) 
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            
+            //Insert the HTML innto the DOM
+            //element.insertAdjacentHTML(position, text);
+            //look up mozilla Adjacent HTML docs  we are using (before)
+
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -99,11 +137,16 @@ var controller = (function(budgetCtrl, UICtrl){
         });
     }
 
+    //this is like the controll center of our app, it tells the other modules what is should do, and then it gets data back to use
     var ctrlAddItem = function(){
-         //1. get the feild input data
-         var input = UICtrl.getInput();
+        var input, newItem; 
+        
+        //1. get the feild input data
+         input = UICtrl.getInput();
          console.log(input)
-        //2. Add the item to the budget controller 
+        
+         //2. Add the item to the budget controller 
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //3. Add the item to the UI
         //4. Calculate the budget
         //5. Display the budget on the UI
